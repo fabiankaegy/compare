2cb838a1665f3042dfee27fda748636ad0712322
Fabian Kägy <fabian@arvernus.info>
2019-08-18 21:27:41 +0200

    Initial commit

--- index.js
+++
@@ -1,6 +0,0 @@
-export const compareProp = (propName, searchTerm) => item => {
-    if (item[propName] === undefined) {
-        throw Error(`The prop "${propName}" does not exist.`);
-    }
-    return item[propName] === searchTerm
-}
